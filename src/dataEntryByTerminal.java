f9ad4a4ce7acc78948939a68086af8143454c665
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-07-23 01:19:01 -0400

    add data entry - terminal

---
+++ src/dataEntryByTerminal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class dataEntryByTerminal {
+    public static void main(String[] args) {
+    Scanner entry = new Scanner(System.in);
+        System.out.println("Enter an integer number: ");
+
+    int decimalValue = entry.nextInt();
+        System.out.println("Decimal system: " + decimalValue); // Decimal system: 450
+
+        String binarySystem = Integer.toBinaryString(decimalValue);
+        System.out.println("Binary System: " + binarySystem);   // Binary System: 111000010
+
+        String hexSystem = Integer.toHexString(decimalValue);
+        System.out.println("Hexadecimal System: " + hexSystem); // Hexadecimal System: 1c2
+
+        String octalSystem = Integer.toOctalString(decimalValue);
+        System.out.println("Octal System: " + octalSystem); // Octal System: 702
+
+    }
+}
