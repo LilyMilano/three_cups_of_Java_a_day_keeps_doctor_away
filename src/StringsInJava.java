231b77a0aabf094d20707b2d7c2c380aca57a918
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-07-25 01:51:11 -0400

    add string methods in Java

--- src/StringsInJava.java
+++ src/StringsInJava.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class StringsInJava {
     public static void main(String[] args) {
         String bookName = "\"Tokio Blues\"";
@@ -26,6 +28,53 @@ public class StringsInJava {
         // replace()
         greet = greet.replace("o", "O");
         System.out.println(greet);  // HellO, Liliana. My favOrite bOOk is "TOkiO Blues". What is yOurs?
+
+        // STRING METHODS .............................................................................................
+
+        String firstName = "Sabrina";
+        System.out.println(firstName.toUpperCase(Locale.ROOT)); // SABRINA
+        System.out.println(firstName.toLowerCase(Locale.ROOT)); // sabrina
+        System.out.println(firstName.equals("sabrina"));    // false
+        System.out.println(firstName.equalsIgnoreCase("sabrina"));  // true
+        //.............................................................................................................
+        System.out.println(firstName.compareTo("sabrina")); // -32
+        /* compareTo()
+        * Compares two strings lexicographically. The comparison is based on the Unicode value of each
+        * character in the strings. The character sequence represented by this String object is compared
+        * lexicographically to the character sequence represented by the argument string.
+        * The result is a negative integer if this String object lexicographically precedes the argument string.
+        * The result is a positive integer if this String object lexicographically follows the argument string.
+        * The result is zero if the strings are equal; compareTo returns 0 exactly when the equals(Object) method would return true.
+        * */
+        System.out.println(firstName.compareToIgnoreCase("sabrina"));   // 0
+        //.............................................................................................................
+
+        System.out.println(firstName.charAt(0));    // S -first char (index=0)
+        System.out.println(firstName.charAt(firstName.length()-1));    // a -last char (index= .length()-1)
+        System.out.println(firstName.replace("a", "#"));    // S#brin#
+        System.out.println(firstName.lastIndexOf("a")); // 6
+        System.out.println(firstName.indexOf("i")); // 4
+        System.out.println(firstName.startsWith("Sabr"));   // true
+        System.out.println(firstName.startsWith("ina"));    // false
+        System.out.println(firstName.endsWith("ina"));      // true
+        System.out.println(firstName.contains("bri"));      // true
+
+        //.............................................................................................................
+
+        System.out.println(firstName.trim());   //Sabrina
+        /*
+        * Returns a string whose value is this string, with all leading and trailing space removed, where space is
+        * defined as any character whose codepoint is less than or equal to 'U+0020' (the space character).
+        */
+        //.............................................................................................................
+
+        // Example: Get file extension
+
+        String file = "random-image.png";
+        int i = file.lastIndexOf(".");
+
+        System.out.println(file.length());  // 16
+        System.out.println(file.substring(i));  // .png
 
     }
 }
