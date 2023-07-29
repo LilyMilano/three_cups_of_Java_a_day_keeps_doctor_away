f31926752a67c5a3ba45fe86106d92c345545423
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-07-28 22:15:25 -0400

    add Java Book - first snippets

---
+++ src/JavaBook/FirstSnippets.java
@@ -0,0 +1,13 @@
+package JavaBook;
+
+public class FirstSnippets {
+    public static void main(String[] args) {
+
+        //Pay Barry
+        double checkAmount = 1257.03;
+        String author = "Barry Burd";
+        System.out.println("Pay to the order of " + author + ": $" + checkAmount);
+
+
+    }
+}
