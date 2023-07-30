dc1965ec4de889f8570b35fe6416572efa7c58d8
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-07-30 01:59:25 -0400

    add 'looking for a vacant room' program

--- src/JavaBook/FirstSnippets.java
+++ src/JavaBook/FirstSnippets.java
@@ -8,6 +8,26 @@ public class FirstSnippets {
         String author = "Barry Burd";
         System.out.println("Pay to the order of " + author + ": $" + checkAmount);
 
+        // Looking for a vacant room
+        int guests;
 
+        for(int roomNum = 1; roomNum <= 100; roomNum++) {
+
+            if(roomNum == 11 || roomNum == 22 || roomNum == 33 || roomNum == 44 ||
+                    roomNum == 55 || roomNum == 66 || roomNum == 77 || roomNum == 88 ||
+                    roomNum == 99) {
+
+                guests = 0;
+
+            } else {
+                guests = 2;
+            }
+
+            if(guests == 0) {
+                System.out.println("Room " + roomNum + " is vacant");
+            } else {
+                System.out.println("Room " + roomNum + " is occupied");
+            }
+        }
     }
 }
