39e5bbb0306ad524d380c77a907ff111677c65bd
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-01 23:54:31 -0400

    add javadocs comments to generate html file

---
+++ src/Java_Barry_Burd/Displayer.java
@@ -0,0 +1,24 @@
+package Java_Barry_Burd;
+
+// Javadoc comments
+
+/**
+ * The Displayer class displays text
+ * on the computer screen.
+ *
+ * @author Barry Burd
+ * @version 1.0 1/24/22
+ * @see java.lang.System
+ */
+public class Displayer {
+
+    /**
+     * The main method is where
+     * execution of the code begins.
+     *
+     * @param args (See chapter 11.)
+     */
+    public static void main(String[] args) {
+        System.out.println("No more baked beans!");
+    }
+}
