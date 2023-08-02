47028d061d434101e42b732602bf489f775b35b9
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-02 02:59:53 -0400

    add variables exercises

--- src/Java_Barry_Burd/Displayer.java
+++ src/Java_Barry_Burd/Displayer.java
@@ -20,5 +20,38 @@ public class Displayer {
      */
     public static void main(String[] args) {
         System.out.println("No more baked beans!");
+
+        // Using variables
+        double amountInAccount = 50.22;
+        amountInAccount +=  1000000.00;
+            System.out.println("You have $" + amountInAccount + " in your account.");
+            // Output: You have $1000050.22 in your account.
+        amountInAccount = 2000000.00;
+            System.out.println("Now you have even more! You have $" + amountInAccount + " in your account.");
+            // Output: Now you have even more! You have $2000000.0 in your account.
+
+        float amountInSavingAccount;
+        amountInSavingAccount = 50.22F;
+        amountInSavingAccount += 1000000.00F;
+            System.out.println("You have $" + amountInSavingAccount + " in your saving account.");
+            // Output: You have $1000050.25 in your saving account.
+
+        // Elevator Fitter:
+        int weightOfAPerson = 150;
+        int elevatorWeightLimit = 1400;
+        int numberOfPeople;
+
+        numberOfPeople =  elevatorWeightLimit / weightOfAPerson;
+            System.out.println("You can fit " + numberOfPeople + " people on the elevator.");
+            // Output: You can fit 9 people on the elevator.
+
+        // Leap year anniversary:
+        int years = 8;
+        int numberOfAnniversaries;
+        int anniversaryEvery = 4;
+
+        numberOfAnniversaries = years / anniversaryEvery;
+            System.out.println("Number of anniversaries: " + numberOfAnniversaries + ".");
+            // Output: Number of anniversaries: 2.
     }
 }
