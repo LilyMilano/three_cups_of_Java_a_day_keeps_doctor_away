c216b86450d3c39ab2d590228299833755e8c39c
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-09 23:51:25 -0400

    add chapter 6: 'for' loop practices

--- src/Java_Barry_Burd/Chapter6.java
+++ src/Java_Barry_Burd/Chapter6.java
@@ -108,9 +108,154 @@ public class Chapter6 {
         out.println("The largest number entered was: " + largestNumber + ".");
         out.println();
 
+        // keyboard.close();
+
+        // .............................................................................................................
+        // FOR LOOP:
+        // for ( initialization ; expression ; update )
+
+        for (int count = 1; count <= 10; count++) {
+            out.println("The value of count is: " + count + ".");
+        }
+        out.println("Done!");
+        out.println();
+
+        // .............................................................................................................
+
+       /* The program in Listing 6-3 prints all the lyrics of
+        the hit single “Al’s All Wet.” (You can find the lyrics in Chapter 5.)*/
+
+        String intro = """
+                Al's all wet. Oh, why is Al all wet? Oh,
+                Al's all wet 'cause he's standing in the rain.""";
+
+        for (int verse = 1; verse <= 3; verse++) {
+            out.println(intro);
+            out.println("Why is Al out in the rain?");
+
+            out.println(switch (verse) { // switch expression
+                case 1 -> "That's because he has no brain.";
+                case 2 -> "That's because he is a pain.";
+                case 3 -> "'Cause this is the last refrain.";
+                default -> "";
+            });
+
+            switch (verse) { // switch statement with fall-through
+                case 3:
+                    out.println("Last refrain, last refrain,");
+                case 2:
+                    out.println("He's a pain, he's a pain,");
+                case 1:
+                    out.println("Has no brain, has no brain,");
+            }
+            out.println("""
+                    In the rain, in the rain.
+                    Oh, oh, oh, oh""");
+            out.println();
+        }
+        out.println(intro);
+        out.println();
+
+        /*Output:
+
+        Al's all wet. Oh, why is Al all wet? Oh,
+        Al's all wet 'cause he's standing in the rain.
+        Why is Al out in the rain?
+        That's because he has no brain.
+        Has no brain, has no brain,
+        In the rain, in the rain.
+        Oh, oh, oh, oh
+
+        Al's all wet. Oh, why is Al all wet? Oh,
+        Al's all wet 'cause he's standing in the rain.
+        Why is Al out in the rain?
+        That's because he is a pain.
+        He's a pain, he's a pain,
+        Has no brain, has no brain,
+        In the rain, in the rain.
+        Oh, oh, oh, oh
+
+        Al's all wet. Oh, why is Al all wet? Oh,
+        Al's all wet 'cause he's standing in the rain.
+        Why is Al out in the rain?
+        'Cause this is the last refrain.
+        Last refrain, last refrain,
+        He's a pain, he's a pain,
+        Has no brain, has no brain,
+        In the rain, in the rain.
+        Oh, oh, oh, oh
+
+        Al's all wet. Oh, why is Al all wet? Oh,
+        Al's all wet 'cause he's standing in the rain.*/
+
+
+        // .............................................................................................................
+        // FOR AMOUR:
+        for (int i = 0, j = 10; i < j; i++, j--) {
+            out.println(i + " " + j);
+        }
+        out.println();
+
+            /* Output:
+            0 10
+            1 9
+            2 8
+            3 7
+            4 6*/
+
+        // .............................................................................................................
+        // COLLECTING VALUES: accumulator
+        int total = 0;
+
+        for (int n = 0; n < 10; n++) {
+            total += n;
+            out.println("When n = " + n + ", the accumulated sum is " + total);
+        }
+        out.println("Total: " + total);
+        out.println();
+
+        /* Output:
+        When n = 0, the accumulated sum is 0
+        When n = 1, the accumulated sum is 1
+        When n = 2, the accumulated sum is 3
+        When n = 3, the accumulated sum is 6
+        When n = 4, the accumulated sum is 10
+        When n = 5, the accumulated sum is 15
+        When n = 6, the accumulated sum is 21
+        When n = 7, the accumulated sum is 28
+        When n = 8, the accumulated sum is 36
+        When n = 9, the accumulated sum is 45
+        Total: 45
+        */
+
+        // .............................................................................................................
+        // FACTORIAL:
+
+        /*Write a program in which the user types a positive int value (call it x), and Java
+        displays the value of x! as its output.*/
+
+        keyboard = new Scanner(in);
+        out.println("Enter a positive integer number (x), please.");
+        int x = keyboard.nextInt();
+        int factorial = 1;
+
+        for (int z = 1; z <= x; z++) {
+            factorial *= z;
+        }
+
+        out.println(x + " factorial equals to: " + factorial);
+
+        out.println();
         keyboard.close();
 
         // .............................................................................................................
 
+        // SEEING STARS:
+        for (int row = 0; row < 5; row++) {
+            for (int column = 0; column < 5; column++) {
+                out.print("*");
+            }
+            out.println();
+        }
     }
 }
