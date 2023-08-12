3cb360236f41ef103abb38f5c32f50c762d1ab04
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-11 22:43:49 -0400

    add class 'Account' with its fields and class 'UseAccount' to instance two Objects from 'Account

---
+++ src/Java_Barry_Burd/Chapter7_Account.java
@@ -0,0 +1,7 @@
+package Java_Barry_Burd;
+
+public class Chapter7_Account {
+    String name;
+    String address;
+    double balance;
+}
---
+++ src/Java_Barry_Burd/Chapter7_UseAccount.java
@@ -0,0 +1,44 @@
+package Java_Barry_Burd;
+
+import static java.lang.System.out;
+public class Chapter7_UseAccount {
+    public static void main(String[] args) {
+        // Declaring and Initializing two new Objects instances of Chapter7_Account class:
+        Chapter7_Account myAccount = new Chapter7_Account();
+        Chapter7_Account yourAccount = new Chapter7_Account();
+
+        // Giving values to constructor Fields of each Object:
+        myAccount.name = "Jane Spears";
+        myAccount.address = "222 Cyberspace Lane";
+        myAccount.balance = 24.02;
+
+        yourAccount.name = "Joey King";
+        yourAccount.address = "111 Consumer Street";
+        yourAccount.balance = 55.03;
+
+        // Using Objects Fields - Dot Notation
+        // Printing values:
+
+        out.print(myAccount.name);
+        out.print(" (");
+        out.print(myAccount.address);
+        out.print(") has $");
+        out.println(myAccount.balance);
+        out.println();
+
+        out.print(yourAccount.name);
+        out.print(" (");
+        out.print(yourAccount.address);
+        out.print(") has $");
+        out.println(yourAccount.balance);
+        out.println();
+
+        /* Output:
+        Jane Spears (222 Cyberspace Lane) has $24.02
+
+        Joey King (111 Consumer Street) has $55.03
+        */
+
+
+    }
+}
