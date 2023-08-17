0da1127c316db5c40c291cfb7bee273eb86776f0
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-17 06:13:39 -0400

    add 'printf' and 'for loop' exercise

--- src/Java_Barry_Burd/chapter7_printf.java
+++ src/Java_Barry_Burd/chapter7_printf.java
@@ -1,7 +1,10 @@
 package Java_Barry_Burd;
+import java.time.LocalDateTime;
 import java.util.Date;
-
+import java.util.Scanner;
+import static java.lang.System.in;
 import static java.lang.System.out;
+
 public class chapter7_printf {
     public static void main(String[] args) {
         out.printf("%s%d%s", ">>", 7, "<<\n");
@@ -65,7 +68,8 @@ public class chapter7_printf {
         out.printf("|  %-8s | %-+,15d |%n", "%-+,15d", 123457890);
         out.printf("|  %-8s | %0,15d |%n", "%0,15d", 123457890);
 
-     /* -------------------------------
+        /*
+        -------------------------------
                 Java int printf chart
                 (number: 123457890)
         -------------------------------
@@ -109,6 +113,22 @@ public class chapter7_printf {
         out.printf("|  %-8s | %015.3f |%n", "%015.3f", 12345.12345);
         out.printf("-------------------------------%n%n");
 
+        /*
+        Output:
+        -------------------------------
+            Java double printf chart
+            (number: 12345.12345)
+        -------------------------------
+        |  PATTERN  |      RESULT     |
+        -------------------------------
+        |  %f       | 12345.123450 |
+        |  %15f     |    12345.123450 |
+        |  %-15f    | 12345.123450    |
+        |  %-15.3f  | 12345.123       |
+        |  %015.3f  | 00000012345.123 |
+        -------------------------------
+        */
+
         //..............................................................................................................
         /*Format a Java char or boolean with printf
         In Java, a char uses the %c specifier. A boolean values uses %b.
@@ -135,11 +155,158 @@ public class chapter7_printf {
         an additional date detail parameter.*/
 
         Date d = new Date();
-        out.printf("Local Time: %tH %tM %tS %tz %n%n", d, d, d, d);
+        out.printf("Current Hour: %tH %tM %tS %tz %n%n", d, d, d, d);
         /*Output:
-        Local Time: 23 23 05 -0400
+        Current Hour: 23 23 05 -0400
         */
 
+        //..............................................................................................................
+        /*Format local time in Java with printf:
+        Modern programs prefer to use the LocalDateTime object over java.util.Date*/
 
+        LocalDateTime dt = LocalDateTime.now();
+        out.printf("Local Date Time: %tA, %tB %te, %tY %tT %tp %n%n", dt, dt, dt, dt, dt, dt);
+        /* Output:
+        Local Date Time: Thursday, August 17, 2023 00:01:10 am
+        */
+
+        //..............................................................................................................
+
+        out.printf("--------------------------------%n");
+        out.printf("      Java's Primitive Types    %n");
+        out.printf("      (printf table example)    %n");
+
+        out.printf("--------------------------------%n");
+        out.printf("| %-10s | %-8s | %4s |%n", "CATEGORY", "NAME", "BITS");
+        out.printf("--------------------------------%n");
+
+        out.printf("| %-10s | %-8s | %04d |%n", "Floating", "double",  64);
+        out.printf("| %-10s | %-8s | %04d |%n", "Floating", "float",   32);
+        out.printf("| %-10s | %-8s | %04d |%n", "Integral", "long",    64);
+        out.printf("| %-10s | %-8s | %04d |%n", "Integral", "int",     32);
+        out.printf("| %-10s | %-8s | %04d |%n", "Integral", "char",    16);
+        out.printf("| %-10s | %-8s | %04d |%n", "Integral", "short",   16);
+        out.printf("| %-10s | %-8s | %04d |%n", "Integral", "byte",    8);
+        out.printf("| %-10s | %-8s | %04d |%n", "Boolean",  "boolean", 1);
+
+        out.printf("--------------------------------%n%n");
+
+        /*
+        Output:
+        --------------------------------
+            Java's Primitive Types
+            (printf table example)
+        --------------------------------
+        | CATEGORY   | NAME     | BITS |
+        --------------------------------
+        | Floating   | double   | 0064 |
+        | Floating   | float    | 0032 |
+        | Integral   | long     | 0064 |
+        | Integral   | int      | 0032 |
+        | Integral   | char     | 0016 |
+        | Integral   | short    | 0016 |
+        | Integral   | byte     | 0008 |
+        | Boolean    | boolean  | 0001 |
+        --------------------------------
+        */
+
+        //..............................................................................................................
+
+        out.printf("-------------------------------------------------------%n");
+        out.printf("                 Java printf cheatsheet     %n");
+        out.printf("                 (printf table example)     %n");
+
+        out.printf("-------------------------------------------------------%n");
+        out.printf("| %-10s | %-20s | %15s |%n", "PATTERN", "DATA", "Printf output");
+        out.printf("-------------------------------------------------------%n");
+
+        out.printf("| %-10s | %-20s | %s | %n", "%s", "Java", "Java");
+        out.printf("| %-10s | %-20s | %15s | %n", "%15s", "Java", "Java");
+        out.printf("| %-10s | %-20s | %-15s | %n", "%-15s", "Java", "Java");
+        out.printf("| %-10s | %-20s | %d | %n", "%d", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %,15d | %n", "%,15d", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %+,15d | %n", "%+,15d", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %-+,15d | %n", "%-+,15d", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %0,15d | %n", "%0,15d", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %15o | %n", "%15o", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %15x | %n", "%15x", 123457890, 123457890);
+        out.printf("| %-10s | %-20s | %15f | %n", "%15f", 12345.123450, 12345.123450);
+        out.printf("| %-10s | %-20s | %-15.3f | %n", "%-15.3f", 12345.123450, 12345.123450);
+        out.printf("| %-10s | %-20s | %015.3f | %n", "%015.3f", 12345.123450, 12345.123450);
+        out.printf("| %-10s | %-20s | %e | %n", "%e", 12345.123450, 12345.123450);
+        out.printf("| %-10s | %-20s | %.2e | %n", "%.2e", 12345.123450, 12345.123450);
+        out.printf("| %-10s | %-20s | %7tH %<-7tM | %n", "%7tH %<-7tM", new Date(), new Date());
+        out.printf("| %-10s | %-20s | %15tT | %n", "%15tT", LocalDateTime.now(), LocalDateTime.now());
+        out.printf("-------------------------------------------------------%n%n");
+
+        /* Output:
+        -------------------------------------------------------
+                    Java printf cheatsheet
+                    (printf table example)
+        -------------------------------------------------------
+        | PATTERN    | DATA                 |   Printf output |
+        -------------------------------------------------------
+        | %s         | Java                 | Java |
+        | %15s       | Java                 |            Java |
+        | %-15s      | Java                 | Java            |
+        | %d         | 123457890            | 123457890 |
+        | %,15d      | 123457890            |     123,457,890 |
+        | %+,15d     | 123457890            |    +123,457,890 |
+        | %-+,15d    | 123457890            | +123,457,890    |
+        | %0,15d     | 123457890            | 0000123,457,890 |
+        | %15o       | 123457890            |       726750542 |
+        | %15x       | 123457890            |         75bd162 |
+        | %15f       | 12345.12345          |    12345.123450 |
+        | %-15.3f    | 12345.12345          | 12345.123       |
+        | %015.3f    | 12345.12345          | 00000012345.123 |
+        | %e         | 12345.12345          | 1.234512e+04 |
+        | %.2e       | 12345.12345          | 1.23e+04 |
+        | %7tH %<-7tM | Thu Aug 17 01:39:07 VET 2023 |      01 39      |
+        | %15tT      | 2023-08-17T01:39:07.613037700 |        01:39:07 |
+        -------------------------------------------------------
+        */
+
+    // HackerRank exercise:
+        /*Every line of input will contain a String followed by an integer.
+        Each String will have a maximum of 10 alphabetic characters, and each integer will be in the
+        inclusive range from 0 to 999.
+        Output Format:
+        In each line of output there should be two columns:
+        The first column contains the String and is left justified using exactly  characters.
+        The second column contains the integer, expressed in exactly  digits; if the original
+        input has less than three digits, you must pad your output's leading digits with zeroes.*/
+
+        Scanner keyboard = new Scanner(in);
+        out.println("Enter three pair of values. E.g:");
+        out.println("winter 500");
+        out.println("summer 999");
+        out.println("solstice 0");
+
+        String[] str = new String[3];
+        int[] numbers = new int[3];
+
+        for(int i = 0; i < 3; i++){
+            str[i] = getValidString(keyboard);
+            numbers[i] = getValidNumber(keyboard);
+        }
+        out.printf("================================%n");
+        for (int i  = 0;  i < 3; i++){
+            System.out.printf("%-15s%03d %n", str[i], numbers[i]);
+        }
+        out.printf("================================%n");
+    }
+    public static String getValidString(Scanner keyboard){
+        String str = keyboard.next();
+        if(str.length() > 10){
+            return str.substring(0, 10);
+        }
+        return str;
+    }
+    public static int getValidNumber(Scanner keyboard){
+        int number = keyboard.nextInt();
+        if(number >= 0 && number <= 999){
+            return number;
+        }
+        else return 0;
     }
 }
