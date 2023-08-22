7d7f52a9940d249a7060bea771a51c8bd5ba7d64
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-22 00:02:15 -0400

    chapter 8: reading files from disk, final settings

--- src/Java_Barry_Burd/chapter8/employee/PayrollBufferedReader.java
+++ src/Java_Barry_Burd/chapter8/employee/PayrollBufferedReader.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class PayrollBufferedReader {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         try (BufferedReader diskReader = new BufferedReader(
                 new FileReader("dataFiles\\EmployeeInfo.txt"))) {
 
