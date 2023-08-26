ee9be7e789728d36f296fe601c85dfbc9110b30b
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-08-26 17:47:03 -0400

    add chapter 9: enum data type and constructors - Temperature Class

---
+++ src/Java_Barry_Burd/chapter9/TempScale.java
@@ -0,0 +1,5 @@
+package Java_Barry_Burd.chapter9;
+
+public enum TempScale { // Declaration of an 'enum' type
+    CELSIUS, FAHRENHEIT, KELVIN, RANKINE, NEWTON, LEIDEN    // Values of TempScale Type
+}
---
+++ src/Java_Barry_Burd/chapter9/Temperature.java
@@ -0,0 +1,40 @@
+package Java_Barry_Burd.chapter9;
+
+public class Temperature {
+
+    // Class Fields:
+    private double number;
+    private TempScale scale;
+
+    // Constructors:
+    public Temperature(){
+        number = 0.0;
+        scale = TempScale.FAHRENHEIT;
+    }
+    public Temperature(double number){
+        this.number = number;
+        scale = TempScale.FAHRENHEIT;
+    }
+    public Temperature(TempScale scale){
+        number = 0.0;
+        this.scale = scale;
+    }
+    public Temperature(double number, TempScale scale){
+        this.number = number;
+        this.scale = scale;
+    }
+
+    // Setters and getters:
+    public void setNumber(double number){
+        this.number = number;
+    }
+    public double getNumber() {
+        return number;
+    }
+    public void setScale(TempScale scale){
+        this.scale = scale;
+    }
+    public TempScale getScale() {
+        return scale;
+    }
+}
