8b4965732f7762b4bffad2717b0a8f083f0ab4a6
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-09-22 18:02:07 -0400

    add code explanations

--- src/Java_Barry_Burd/chapter11/hotel/FindVacancyUsingRWMax.java
+++ src/Java_Barry_Burd/chapter11/hotel/FindVacancyUsingRWMax.java
@@ -11,6 +11,10 @@ public class FindVacancyUsingRWMax {
         RoomWithMax[] room = new RoomWithMax[10];
         int roomNum;
 
+        /*Reading room information from a file: This block of code initializes an array of RoomWithMax objects,
+        reads room information from the "RoomList.txt" file using a Scanner, and assigns the values to the corresponding
+        RoomWithMax objects. This is important for initializing the room data for further processing.*/
+
         Scanner diskScanner = new Scanner(new File("RoomList.txt"));
         for(roomNum = 0; roomNum < 10; roomNum++){
             room[roomNum] = new RoomWithMax();
@@ -18,6 +22,11 @@ public class FindVacancyUsingRWMax {
         }
         diskScanner.close();
 
+        /*User input: This block of code prompts the user for the number of people and smoking preferences.
+        The user's input is crucial for finding a suitable room that matches their preferences.
+        Using keyboard.findWithinHorizon(".", 0) allows the code to read a single character directly,
+        without considering whitespace or other tokens. This can be useful when you specifically want to read
+        a single character as input.*/
         Scanner keyboard = new Scanner(System.in);
         out.print("How many people? ");
         int howMany = keyboard.nextInt();
@@ -25,11 +34,16 @@ public class FindVacancyUsingRWMax {
         boolean wantSmoking = keyboard.findWithinHorizon(".", 0).charAt(0) == 'y';
         keyboard.close();
 
+        /*Finding a vacant room: This block of code iterates through the room array and checks each room's availability,
+        smoking status, and maximum occupancy to find the first vacant room that matches the user's preferences.*/
         roomNum = 0;
         while(roomNum < 10 && (room[roomNum].getGuests() != 0 || room[roomNum].isSmoking() != wantSmoking || room[roomNum].getMaxOccupancy() < howMany)) {
             roomNum++;
         }
 
+        /*Updating room information and saving to file: If a vacant room is found, this block of code updates the
+        room's guest count, creates a PrintStream to write the updated room information to the "RoomList.txt" file,
+        and saves the changes.*/
         if(roomNum == 10){
             out.println("Sorry, no vacancy");
         } else {
