cc6da4a93bcaf5a74d5a13734655dfc4331c72cd
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-11-03 00:58:17 -0400

    minor spacing fixes

--- src/Java_Barry_Burd/chapter14/com/music/main/Main.java
+++ src/Java_Barry_Burd/chapter14/com/music/main/Main.java
@@ -3,17 +3,22 @@ import Java_Barry_Burd.chapter14.com.music.album.Album;
 import Java_Barry_Burd.chapter14.com.music.artist.Artist;
 import Java_Barry_Burd.chapter14.com.music.playlist.Playlist;
 import Java_Barry_Burd.chapter14.com.music.song.Song;
+
 public class Main {
     public static void main(String[] args) {
+
         // Create an artist:....................................................
         Artist artist = new Artist("Shakira");
+
         // Create songs:........................................................
         Song song1 = new Song("Empire", artist, Song.Genre.ROCK);
         Song song2 = new Song("Objection", artist, Song.Genre.POP);
+
         // Create an album:.....................................................
         Album album = new Album("Laundry Service", artist);
         album.getSongs().add(song1);
         album.getSongs().add(song2);
+
         // Create a playlist:...................................................
         Playlist playlist = new Playlist();
         playlist.getItems().add(new Song("Medicine", artist, Song.Genre.COUNTRY));
