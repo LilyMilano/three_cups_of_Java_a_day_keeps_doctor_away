d93487da1ab04fe5d34db2c612a4501dc9065321
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-11-17 01:15:08 -0400

    add chapter 17: retrieve data from a db

---
+++ src/Java_Barry_Burd/chapter17/accounts/GetData.java
@@ -0,0 +1,27 @@
+package Java_Barry_Burd.chapter17.accounts;
+import java.sql.*;
+import java.text.NumberFormat;
+import static java.lang.System.out;
+public class GetData {
+    public static void main(String[] args) {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        final String CONNECTION =
+                "jdbc:mysql://127.0.0.1:3306/accountdatabase?useSSL=false";
+
+        try(Connection conn = DriverManager.getConnection(CONNECTION);
+            Statement statement = conn.createStatement();
+            ResultSet resultset = statement.executeQuery
+                    ("select * from " + "ACCOUNTS")) {
+
+            while (resultset.next()) {
+                out.print(resultset.getString("NAME"));
+                out.print(", ");
+                out.print(resultset.getString("ADDRESS"));
+                out.print(" ");
+                out.println(currency.format(resultset.getFloat("BALANCE")));
+            }
+        } catch (SQLException e) {
+            out.println(e.getMessage());
+        }
+    }
+}
